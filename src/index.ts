b8082a0d280d8952105e6ce814af6eca473a9b10
Shobhnik13 <shobhnikw@gmail.com>
2024-05-09 17:26:35 +0530

    auth apis added

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,71 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings:{
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET:string
   }
 }>()
 
-app.get('/', (c) => {
+app.post('/api/v1/signup', async(c) => {
   const prisma=new PrismaClient({
     datasourceUrl:c.env.DATABASE_URL,
   }).$extends(withAccelerate())  
-  return c.text('Hello Hono!')
+
+  const body=await c.req.json() 
+
+  try{
+    const userExsits=await prisma.user.findUnique({
+      where:{
+        email:body.email
+      }
+    })
+  
+    if(userExsits){
+      c.status(403)
+      return c.json({error:'user already exists'})
+    }
+
+    const user = await prisma.user.create({
+      data: {
+         email: body.email,
+         password: body.password,
+      },
+   });
+   const token= await sign({id: user.id},c.env.JWT_SECRET)
+   return c.json({token})
+  }catch(e){
+    c.status(403)
+    return c.json({error:'Error while signing up!'})
+  }
+
 })
 
+
+app.post('/api/v1/signin',async(c)=>{
+  const prisma=new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body=await c.req.json()
+
+  const user=await prisma.user.findUnique({
+    where:{
+      email:body.email
+    }
+  })
+
+  if(!user){
+    c.status(403)
+    return c.json({error:'user not found'})
+  }
+  const jwt=await sign({id: user.id},c.env.JWT_SECRET)
+  return c.json({jwt})
+
+})
 
 app.get('/test', (c) => {
   return c.text('Hello Hono2!')
