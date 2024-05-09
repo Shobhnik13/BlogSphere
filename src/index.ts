28bb94d154118d548c07eb1185d0bb0a75c94cec
Shobhnik13 <shobhnikw@gmail.com>
2024-05-09 08:57:04 +0530

    ini

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.get('/test', (c) => {
+  return c.text('Hello Hono2!')
+})
+
+
+export default app
